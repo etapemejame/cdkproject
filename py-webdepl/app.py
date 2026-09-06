df9d31ea44572a1d2406725508539a992fcf214d
Emmanuel <etapemejame@hotmail.com>
2026-09-06 12:14:39 -0400

    Added cdk default account to deploy stack

--- py-webdepl/app.py
+++ py-webdepl/app.py
@@ -15,12 +15,12 @@ PyWebdeplStack(app, "PyWebdeplStack",
     # Uncomment the next line to specialize this stack for the AWS Account
     # and Region that are implied by the current CLI configuration.
 
-    env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
+    env=cdk.Environment(account=os.getenv('AWS_ACCOUNT_ID'), region=os.getenv('AWS_REGION')),
 
     # Uncomment the next line if you know exactly what Account and Region you
     # want to deploy the stack to. */
 
-    #env=cdk.Environment(account='123456789012', region='us-east-1'),
+    # env=cdk.Environment(account='12345678910', region='us-west-2'),
 
     # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
     )
