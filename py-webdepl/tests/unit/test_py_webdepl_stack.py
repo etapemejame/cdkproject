dc29719033e2d922cc2a63faabd6f5ccb1742d47
Emmanuel <etapemejame@hotmail.com>
2026-09-07 14:14:47 -0400

    Included unit test

--- py-webdepl/tests/unit/test_py_webdepl_stack.py
+++ py-webdepl/tests/unit/test_py_webdepl_stack.py
@@ -1,15 +1,20 @@
 import aws_cdk as core
 import aws_cdk.assertions as assertions
+import pytest
+from aws_cdk.assertions import Match, Capture
 
 from py_webdepl.py_webdepl_stack import PyWebdeplStack
 
 # example tests. To run these tests, uncomment this file along with the example
 # resource in py_webdepl/py_webdepl_stack.py
-def test_sqs_queue_created():
+@pytest.fixture(scope='session')
+def tests_template():
     app = core.App()
     stack = PyWebdeplStack(app, "py-webdepl")
     template = assertions.Template.from_stack(stack)
+    return template
 
-#     template.has_resource_properties("AWS::SQS::Queue", {
-#         "VisibilityTimeout": 300
-#     })
+def test_lambda_props(tests_template):
+    tests_template.has_resource_properties("AWS::Lambda::Function", {
+        "Runtime": "python3.13"
+    })
